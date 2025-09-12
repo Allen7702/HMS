d3b93030871bc550ff913640ef6607d620dc1c3c
Allen7702 <allen22052000@gmail.com>
2025-09-12 21:22:34 +0300

    feat(bookings): create BookingsPage component for bookings management

---
+++ apps/admin-web/src/app/(pages)/bookings/page.tsx
@@ -0,0 +1,8 @@
+import { Title } from "@mantine/core";
+import React from "react";
+
+export default function BookingsPage() {
+    return (
+            <Title order={1}>Bookings</Title>
+    );
+}
