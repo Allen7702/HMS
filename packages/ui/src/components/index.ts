c36503ccc5bdd3647b5b4c6a7de717b73334df68
Allen7702 <allen22052000@gmail.com>
2025-09-12 22:08:31 +0300

    feat(ui): export HotelSettingsForm and AppThemeIcon components

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,5 +1,7 @@
 export { LoginPage } from './LoginPage';
+export { HotelSettingsForm } from './HotelSettingsForm';
 export { PageLoader } from './Loader';
 export { NotFoundPage } from './NotFoundPage';
 export { NotFound } from './NotFound';
+export { AppThemeIcon } from './ThemeIcon';
 export { showSuccessNotification, showErrorNotification } from './Notification';
