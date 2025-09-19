b392f89bdd994c48767c92c9aefb3213cef8325c
Allen7702 <allen22052000@gmail.com>
2025-09-19 10:56:17 +0300

    refactor(contexts): streamline RootProvider by removing unused websocketUrl prop and enhancing HotelProvider export

--- packages/ui/src/contexts/MantineContext.tsx
+++ packages/ui/src/contexts/MantineContext.tsx
@@ -1,34 +1,39 @@
 'use client';
 
 import React, { ReactNode } from 'react';
-import { Notifications, showNotification } from '@mantine/notifications';
+import { Notifications } from '@mantine/notifications';
+import { DatesProvider } from '@mantine/dates';
+import dayjs from 'dayjs';
+import 'dayjs/locale/en';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+import utc from 'dayjs/plugin/utc';
 import { ThemeProvider } from './ThemeContext';
 import { AuthProvider } from './AuthContext';
 import { NotificationProvider } from './NotificationContext';
 import { AppProvider } from './AppContext';
-import { WebSocketProvider } from './WebSocketContext';
 import { HotelProvider } from './HotelContext';
-
+dayjs.extend(customParseFormat);
+dayjs.extend(utc);
+dayjs.locale('en');
 interface RootProviderProps {
   children: ReactNode;
-  websocketUrl?: string;
 }
 
-export function RootProvider({ children, websocketUrl }: RootProviderProps) {
+export function RootProvider({ children }: RootProviderProps) {
   return (
     <ThemeProvider>
-      <Notifications position="bottom-right" />
-      <AuthProvider>
-        <AppProvider>
-          <NotificationProvider>
-            <HotelProvider>
-              {/* <WebSocketProvider url={websocketUrl}> */}
+      <DatesProvider settings={{ locale: 'en', firstDayOfWeek: 0, weekendDays: [0, 6] }}>
+        <Notifications position="bottom-right" />
+        <AuthProvider>
+          <AppProvider>
+            <NotificationProvider>
+              <HotelProvider>
                 {children}
-              {/* </WebSocketProvider> */}
-            </HotelProvider>
-          </NotificationProvider>
-        </AppProvider>
-      </AuthProvider>
+              </HotelProvider>
+            </NotificationProvider>
+          </AppProvider>
+        </AuthProvider>
+      </DatesProvider>
     </ThemeProvider>
   );
 }
